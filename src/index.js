8aaac67539a64bab9862072668f40361f3ba3869
db05049-creator <db05049@gmail.com>
2026-05-08 12:08:53 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // 여기서 ./App은 App.js 파일을 가리킵니다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
